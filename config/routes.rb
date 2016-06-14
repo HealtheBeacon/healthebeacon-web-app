e6d5b7ad880494422fb871277c5b9ff0b1d4a4e9
Andrew McKittrick <amckit@ksu.edu>
2016-06-14 00:07:44 -0500

    Adding pages for my team

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'splash#view'
    get 'teams' => 'teams#view'
+   get 'myteam' => 'my_team#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
